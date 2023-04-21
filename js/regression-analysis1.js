fe0e0e175a5043f93eec3b06298e5ae7451e48c9
mafeep <123569644+mafeep@users.noreply.github.com>
2023-04-21 18:11:21 -0500

    fixed

--- js/regression-analysis1.js
+++ js/regression-analysis1.js
@@ -39,12 +39,12 @@ function linearRegression(values_x, values_y) {
 
 //For kWh vs Month
 const months = [1, 2, 3, 4, 5];
-const kwhs = [240, 250, 274, 250, 300];
+const kwhs = [133, 140, 164, 173, 144];
 var l = linearRegression(months, kwhs);
 
 //For Residents vs kWh
-const residents = [1, 2, 3, 5, 5, 6, 3, 8, 3, 5];
-const kWhs = [240, 250, 274, 260, 210, 300, 260, 440, 300, 350];
+const residents = [3, 3, 3, 3, 3, 4, 4, 4, 5, 7];
+const kWhs = [93, 79, 115, 144, 53, 221, 137, 140, 192, 475];
 var r = linearRegression(residents, kWhs);
 
 const trace11 = {
